04e315dfa2ae51dd8140cd86c71011016c2a3d2b
Samakshg11 <samakshgarg2005@gmail.com>
2026-05-15 23:53:04 +0530

    feat(artifacts): expose stable id field in JSON responses

--- src/models/artifact.model.js
+++ src/models/artifact.model.js
@@ -13,6 +13,14 @@ const artifactSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    toJSON: {
+      versionKey: false,
+      transform: (_, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        return ret;
+      },
+    },
   }
 );
 
