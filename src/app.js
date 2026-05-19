bd219743e3198f8857bec4162c89c428bce9fb47
Samakshg11 <samakshgarg2005@gmail.com>
2026-05-19 23:50:17 +0530

    chore: reduce express fingerprinting and trust first proxy

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const { name, version } = require("../package.json");
 
 const app = express();
 
+app.disable("x-powered-by");
+app.set("trust proxy", 1);
+
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
