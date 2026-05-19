40ec82ae8adf16ebfc9e85e003f13e4fca357213
Samakshg11 <samakshgarg2005@gmail.com>
2026-05-19 23:50:34 +0530

    feat: add structured validation details in error responses

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,11 +1,13 @@
 module.exports = (err, req, res, next) => {
   let status = err.statusCode || 500;
   let message = err.message || "Internal server error";
+  let details;
   const isDev = process.env.NODE_ENV === "development";
 
   if (err.name === "ValidationError") {
     status = 400;
     message = "Validation failed";
+    details = Object.values(err.errors || {}).map((item) => item.message);
   }
 
   if (err.name === "CastError") {
@@ -16,6 +18,7 @@ module.exports = (err, req, res, next) => {
   if (err.code === 11000) {
     status = 409;
     message = "A record with the same unique value already exists";
+    details = Object.keys(err.keyPattern || {});
   }
 
   if (status >= 500) {
@@ -25,6 +28,7 @@ module.exports = (err, req, res, next) => {
   res.status(status).json({
     success: false,
     message,
+    ...(details && details.length > 0 ? { details } : {}),
     ...(isDev && { stack: err.stack }),
   });
 };
