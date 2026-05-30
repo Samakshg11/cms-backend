5715376fc22583925e95a1f992b5d74afd5f1b3d
Samakshg11 <samakshgarg2005@gmail.com>
2026-05-30 19:00:34 +0530

    chore: add graceful shutdown handlers for server process

--- server.js
+++ server.js
@@ -3,15 +3,39 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 const { getPort, validateEnv } = require("./src/config/env");
 
+let server;
+
+const shutdown = (signal) => {
+  console.log(`Received ${signal}. Shutting down gracefully...`);
+
+  if (!server) {
+    process.exit(0);
+    return;
+  }
+
+  server.close((error) => {
+    if (error) {
+      console.error("Error while closing server", error.message);
+      process.exit(1);
+      return;
+    }
+
+    process.exit(0);
+  });
+};
+
 const startServer = async () => {
   validateEnv();
   await connectDB();
   const port = getPort();
 
-  app.listen(port, () => {
+  server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
 };
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
 
 startServer().catch((error) => {
   console.error("Failed to start server");
