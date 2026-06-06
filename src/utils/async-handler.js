1f740cd0929e092368db6d66c90634c7c4a518db
Dev Automation <dev@example.com>
2026-06-06 23:53:16 +0530

    fix(async-handler): Log unhandled async errors with route context

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,3 +1,12 @@
+const logger = require('../../utils/logger');
+
 module.exports = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+  Promise.resolve(fn(req, res, next)).catch((err) => {
+    try {
+      logger.error('Unhandled async error', req.method, req.originalUrl, err.message);
+    } catch (e) {
+      // ignore logging errors
+    }
+    next(err);
+  });
 };
