77d48c13facbbfa6008efcb8fa874c7d63ed1e5b
Samakshg11 <samakshgarg2005@gmail.com>
2026-07-19 00:29:48 +0530

    fix(logger): fix missing closing parenthesis in error logging function

--- utils/logger.js
+++ utils/logger.js
@@ -13,5 +13,5 @@ function formatLogArg(value) {
 module.exports = {
   info: (...args) => console.log('[INFO]', timestamp(), ...args.map(formatLogArg)),
   warn: (...args) => console.warn('[WARN]', timestamp(), ...args.map(formatLogArg)),
-  error: (...args) => console.error('[ERROR]', timestamp(), ...args.map(formatLogArg)
+  error: (...args) => console.error('[ERROR]', timestamp(), ...args.map(formatLogArg))
 };
