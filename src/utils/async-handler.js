cdb13eb31a8ab16da8d98cb81657146830f016dc
Dev Automation <dev@example.com>
2026-07-19 00:01:17 +0530

    fix(logger): simplify error logging in async handler

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -2,11 +2,7 @@ const logger = require('../../utils/logger');
 
 module.exports = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch((err) => {
-    try {
-      logger.error('Unhandled async error', req.method, req.originalUrl, err.message);
-    } catch (e) {
-      // ignore logging errors
-    }
+    logger.error('Unhandled async error', req.method, req.originalUrl, err);
     next(err);
   });
 };
